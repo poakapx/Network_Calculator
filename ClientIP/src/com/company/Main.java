14e7f5a7dcdf2f0eb50a86ff760ae8b493f50aa3
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2021-10-12 20:32:32 +0300

    Updated-2.

--- ClientIP/src/com/company/Main.java
+++ ClientIP/src/com/company/Main.java
@@ -8,40 +8,34 @@ public class Main
 {
     public static void main(String[] args) throws IOException
     {
-        int port = 8080;
-        String str1 = "2";
-        String str2 = "+";
-        String str3 = "2";
-        System.out.println("Clien start!");
-        Socket socket = new Socket("127.0.0.1", port);
-        BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        PrintWriter pw = new PrintWriter(socket.getOutputStream(), true);
+        System.out.println("Клиент запущен!");
+        int port = 8080; String host = "127.0.0.1";
+        // Первая строка, вторая строка, третья строка
+        String str1; String str2; String str3;
+        // Открыть сокет
+        Socket socket = new Socket(host, port);
+        BufferedReader bufferedreader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintWriter printwriter = new PrintWriter(socket.getOutputStream(), true);
         Scanner scanner = new Scanner(System.in);
+        // Ввод данных
         do
         {
-            //наша строка1
-            System.out.print("Enter message-1 (bye - for exit): ");
-            str1 = scanner.next();
-            pw.println(str1);
-            //наша строка2
-            System.out.print("Enter message-2 (bye - for exit): ");
-            str2 = scanner.next();
-            pw.println(str2);
-            //наша строка3
-            System.out.print("Enter message-3 (bye - for exit): ");
-            str3 = scanner.next();
-            pw.println(str3);
-            //ответ
-            str1 = br.readLine();
-            System.out.println(str1);
-            str2 = br.readLine();
-            System.out.println(str2);
-            str3 = br.readLine();
-            System.out.println(str3);
+            // Первая строка
+            System.out.print("Введи число (bye - выход): ");
+            str1 = scanner.next(); printwriter.println(str1);
+            // Вторая строка
+            System.out.print("Введи +, -, *, /, % или ^ (bye - выход): ");
+            str2 = scanner.next(); printwriter.println(str2);
+            // Третья строка
+            System.out.print("Введи число (bye - выход): ");
+            str3 = scanner.next(); printwriter.println(str3);
+            // Ответ с сервака
+            str1 = bufferedreader.readLine(); System.out.println(str1);
+            str2 = bufferedreader.readLine(); System.out.println(str2);
+            str3 = bufferedreader.readLine(); System.out.println(str3);
         }
         while (!str1.equals("bye") || !str2.equals("bye") || !str3.equals("bye"));
-        br.close();
-        pw.close();
-        socket.close();
+        // Закрыть сокет
+        bufferedreader.close(); printwriter.close(); socket.close();
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,78 +7,47 @@ public class Main
 {
     public static void main(String[] args)
     {
+        System.out.println("Сервак запущен!");
         int port = 8080;
+        // Первая строка, вторая строка, третья строка
+        String str1; String str2; String str3;
+        // Открыть сокет
         try
         {
-            ServerSocket serverSocket = new ServerSocket(port);
+            ServerSocket serversocket = new ServerSocket(port);
             while (true)
             {
-                System.out.println("Waiting for client on port " + port);
-                Socket fromClientSocket = serverSocket.accept();
-                try(Socket localSocket = fromClientSocket; PrintWriter pw = new PrintWriter(localSocket.getOutputStream(),true); BufferedReader br = new BufferedReader(new InputStreamReader(localSocket.getInputStream())))
+                System.out.println("Подключение к порту: " + port + "...");
+                Socket fromClientSocket = serversocket.accept();
+                try(Socket localSocket = fromClientSocket; PrintWriter printwriter = new PrintWriter(localSocket.getOutputStream(),true); BufferedReader br = new BufferedReader(new InputStreamReader(localSocket.getInputStream())))
                 {
-                    String str1;
-                    String str2;
-                    String str3;
+                    // Ввод данных
                     while ((str1 = br.readLine()) != null && (str2 = br.readLine()) != null && (str3 = br.readLine()) != null)
                     {
-                        if (str1.equals("bye") && str2.equals("bye") && str3.equals("bye"))
-                        {
-                            pw.println("good bye");
-                            break;
-                        }
+                        if (str1.equals("bye") || str2.equals("bye") || str3.equals("bye"))
+                        { printwriter.println("Прощай!"); break; }
                         else
+                        {
+                            // Первая строка, вторая строка, третья строка
+                            System.out.println("1-е сообщение: " + str1);
+                            System.out.println("2-е сообщение: " + str2);
+                            System.out.println("3-е сообщение: " + str3);
+                            printwriter.println("Сообщения от клиента на сервак доставлены!");
+                            switch (str2)
                             {
-                                System.out.println("The message-1 is " + str1 + "!");
-                                System.out.println("The message-2 is " + str2 + "!");
-                                System.out.println("The message-3 is " + str3 + "!");
-                                pw.println("Message on server!");
-                                switch (str2)
-                                {
-                                    case "+":
-                                        {
-                                            System.out.print("Answer: ");
-                                            System.out.println(Integer.parseInt(str1) + Integer.parseInt(str3));
-                                            break;
-                                        }
-                                        case "-":
-                                            {
-                                                System.out.print("Answer: ");
-                                                System.out.println(Integer.parseInt(str1) - Integer.parseInt(str3));
-                                                break;
-                                            }
-                                            case "*":
-                                                {
-                                                    System.out.print("Answer: ");
-                                                    System.out.println(Integer.parseInt(str1) * Integer.parseInt(str3));
-                                                    break;
-                                                }
-                                                case "%":
-                                                {
-                                                    System.out.print("Answer: ");
-                                                    System.out.println(Integer.parseInt(str1) % Integer.parseInt(str3));
-                                                    break;
-                                                }
-                                                case "^":
-                                                {
-                                                    System.out.print("Answer: ");
-                                                    System.out.println(Integer.parseInt(str1) ^ Integer.parseInt(str3));
-                                                    break;
-                                                }
-                                    }
-                                }
-
+                                case "+": { System.out.print("Ответ: "); System.out.println(Integer.parseInt(str1) + Integer.parseInt(str3)); break; }
+                                case "-": { System.out.print("Ответ: "); System.out.println(Integer.parseInt(str1) - Integer.parseInt(str3)); break; }
+                                case "*": { System.out.print("Ответ: "); System.out.println(Integer.parseInt(str1) * Integer.parseInt(str3)); break; }
+                                case "/": { System.out.print("Ответ: "); System.out.println(Integer.parseInt(str1) / Integer.parseInt(str3)); break; }
+                                case "%": { System.out.print("Ответ: "); System.out.println(Integer.parseInt(str1) % Integer.parseInt(str3)); break; }
+                                case "^": { System.out.print("Ответ: "); System.out.println(Integer.parseInt(str1) ^ Integer.parseInt(str3)); break; }
+                            }
                         }
+                    }
                 }
-                catch(IOException ex)
-                {
-                    ex.printStackTrace(System.out);
-                }
+                catch(IOException ex) { ex.printStackTrace(System.out); }
             }
         }
-        catch (IOException ex)
-        {
-            ex.printStackTrace(System.out);
-        }
+        catch (IOException ex) { ex.printStackTrace(System.out); }
     }
 }
